d0d00ef23cdd41f66ca0c59ea8a144e8040c57f9
Gibexz <andrewetedu@gmail.com>
2023-08-01 00:55:08 +0100

    Task 12 - Update app.py with CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,13 @@ module: app.py
 
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
+from flasK_cors import CORS
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
 
 @app.teardown_appcontext
 def teardown(exception):
